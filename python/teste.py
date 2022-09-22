25b42723f0763584adc675615bd99d7f9bece748
falqtte <matheusfalquette@hotmail.com>
2022-09-22 11:22:01 -0300

    folder organization

--- python/teste.py
+++ python/teste.py
@@ -1,3 +1,3 @@
-print("exemplo\n")
-print("banana\n\n\n\nkiwi")
-# \n significa "nova linha", pega bastante da história da computação e como era feito antigamente nas máquinas de escerver
+import os
+clear = lambda: os.system('clear')
+clear()
